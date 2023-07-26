30c79f4e38965bde0e01b6a13f549d4d89d90f7f
Abayomi Aluko <franicons@gmail.com>
2023-07-26 03:20:29 +0100

    update development

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,25 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 const { loadPlanetData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb://localhost:27016'
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('mongodb connection ready');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
 async function startServer () {
+    await mongoose.connect(MONGO_URL);
     await loadPlanetData();
 
     server.listen(PORT, () => {
